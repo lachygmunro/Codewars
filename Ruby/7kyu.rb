e56fee5ae71b90074570952eb99c6779e763e621
Lachlan Munro <lachlangmunro@gmail.com>
2022-03-28 23:10:18 +1100

    Completed further ruby practice.

--- Ruby/7kyu.rb
+++ Ruby/7kyu.rb
@@ -66,3 +66,76 @@ def is_sorted_and_how(arr)
     'no'
   end
 end
+
+# 7
+# Make a program that filters a list of strings and returns a list with only your friends name in it.
+# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not your friend.
+
+# def friend(friends)
+#   friends.select { |name| name.length == 4 }
+# end
+
+def friend(friends)
+  friends.select do |friend|
+    friend.match(/^\w{4}$/)
+    end
+end
+
+# 8
+# There is a war and nobody knows - the alphabet war!
+# There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+# Task
+# Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+# The left side letters and their power:
+#  w - 4
+#  p - 3
+#  b - 2
+#  s - 1
+# The right side letters and their power:
+#  m - 4
+#  q - 3
+#  d - 2
+#  z - 1
+# The other letters don't have power and are only victims.
+
+def alphabet_war(fight)
+  left = fight.scan(/[wpbs]/)
+  right = fight.scan(/[mqdz]/)
+
+  leftscore = 0
+  left.each do |letter|
+    case letter
+      when "w"
+        leftscore += 4
+      when "p"
+        leftscore += 3
+      when "b"
+        leftscore += 2
+      when "s"
+        leftscore += 1
+    end
+  end
+
+  rightscore = 0
+  right.each do |letter|
+    case letter
+      when "m"
+        rightscore += 4
+      when "q"
+        rightscore += 3
+      when "d"
+        rightscore += 2
+      when "z"
+        rightscore += 1
+    end
+  end
+
+  if leftscore > rightscore
+    "Left side wins!"
+  elsif leftscore < rightscore
+    "Right side wins!"
+  else
+    "Let's fight again!"
+  end
+end
