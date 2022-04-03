2eb24b14a3d47c45ac6bcafa7a16d839a6507c22
Lachlan Munro <lachlangmunro@gmail.com>
2022-04-03 23:20:58 +1000

    Completed challenge.

--- Ruby/7kyu.rb
+++ Ruby/7kyu.rb
@@ -171,3 +171,18 @@ def calc(operator, first, second)
   when '//' then (first / second)
   end
 end
+
+# 10
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible,
+# containing distinct letters - each taken only once - coming from s1 or s2.
+
+# def longest(a1, a2)
+#   str = a1 + a2
+#   str.split('').sort.uniq.join('')
+# end
+
+# refactored. select unique before sorting.
+def longest(a1, a2)
+  str = a1 + a2
+  str.split('').uniq.sort.join('')
+end
