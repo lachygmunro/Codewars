21e82ba4416a8eda6383fec65762906a633feebd
Lachlan Munro <lachlangmunro@gmail.com>
2022-04-05 16:26:33 +1000

    Completed new challenge.

--- Ruby/7kyu.rb
+++ Ruby/7kyu.rb
@@ -78,7 +78,7 @@ end
 def friend(friends)
   friends.select do |friend|
     friend.match(/^\w{4}$/)
-    end
+  end
 end
 
 # 8
@@ -185,4 +185,17 @@ end
 def longest(a1, a2)
   str = a1 + a2
   str.split('').uniq.sort.join('')
+end
+
+# 11
+# You are going to be given a word. Your job is to return the middle character of the word. If the word's
+# length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+def get_middle(s)
+  length = s.length
+  if length.even?
+    s[length / 2 - 1...length / 2 + 1]
+  else
+    s[length / 2]
+  end
 end
